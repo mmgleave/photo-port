184aaa63fb35d75a8721159fda7f42eaf3e0c69e
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-23 15:38:44 -0600

    modal tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,4 +1,3 @@
-// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
