1dc27ec58034bb17fb11d3f090ec622905bfb587
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-23 13:00:42 -0600

    form submission and validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+
+    const { name, email, message } = formState
+
+    function handleChange(event) {
+        setFormState({ ...formState, [event.target.name]: event.target.value })
+    };
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <section>
             <h1>Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="name">Email Address:</label>
-                    <input type="text" name="email" />
+                    <input type="text" defaultValue={email} onChange={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <input type="text" name="message" rows="5" />
+                    <textarea type="text" defaultValue={message} onChange={handleChange} name="message" rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
